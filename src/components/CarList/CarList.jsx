24f1c74cc725aa7d7405e76718ad9023edc08413
Trey Shelton <trey@basecampcodingacademy.org>
2024-10-17 19:08:13 -0500

    fixed error in CarList.jsx

--- src/components/CarList/CarList.jsx
+++ src/components/CarList/CarList.jsx
@@ -14,7 +14,7 @@ function CarList({ cars, onDeleteCar, onUpdateCar }) {
             car={car}
             onDelete={() => onDeleteCar(car.id)}
             onUpdate={onUpdateCar}
-            onCreate={onCreateCar}
+            // onCreate={onCreateCar}
           />
         ))}
       </div>
